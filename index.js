e909b6bfc102741101a50759cecda506c8153de9
Justin Sohn <jusohn2015@gmail.com>
2021-08-11 10:43:04 -0400

    commit tree e0fb125cebb9eba3bdb60197a99fcc41d4920ff5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo');
+}
+
+function appendCat(name){
+    const newCats = [...cats, 'Broom'];
+    return newCats;
+}
+
+function prependCat(name){
+    const newerCats = ['Arnold', ...cats];
+    return newerCats;
+}
+
+function removeLastCat(){
+    const lastCat = cats.slice(0,2);
+    return lastCat;
+}
+
+function removeFirstCat(){
+    const firstCat = cats.slice(1,3);
+    return firstCat;
+}
